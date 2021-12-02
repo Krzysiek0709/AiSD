6470ad7b047ee1c9763e7781aeefcdc6956a708d
Krzysiek0709 <76214275+Krzysiek0709@users.noreply.github.com>
2021-12-02 17:43:14 +0100

    Update btree.py

--- BinaryTree/btree.py
+++ BinaryTree/btree.py
@@ -1,1 +1,39 @@
+from typing import Any, Callable
 
+
+class BinaryNode:
+    def __init__(self, value: Any):
+        self.value = value
+        self.left_child = None
+        self.right_child = None
+
+    def PrintTree(self):
+        if self.left_child:
+            self.left_child.PrintTree()
+        print(self.value)
+        if self.right_child:
+            self.right_child.PrintTree()
+
+
+    def is_leaf(self):
+        return self.left_child == None and self.right_child == None
+
+
+    def add_left_child(self,value:Any):
+        if self.left_child is None:
+            self.left_child = BinaryNode(value)
+        else:
+            self.right_child.add_right_child(value)
+
+
+    def add_right_child(self,value:Any):
+        if self.right_child is None:
+            self.right_child = BinaryNode(value)
+        else:
+            self.right_child.add_right_child(value)
+
+
+root = BinaryNode(20)
+root.add_left_child(5)
+root.add_right_child(6)
+root.PrintTree()
